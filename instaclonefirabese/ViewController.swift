f6d84c5dba25fa27de79fc815546f1d30259e1fe
Furkan Demirel <furkandemireleng@gmail.com>
2019-11-15 19:25:08 +0300

    Initial Commit

---
+++ instaclonefirabese/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instaclonefirabese
+//
+//  Created by MacxbookPro on 15.11.2019.
+//  Copyright © 2019 MacxbookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
