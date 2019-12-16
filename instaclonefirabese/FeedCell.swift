a6cd4ee58938a0edb9c4052a27bfe4cf320c71bd
Furkan Demirel <furkandemireleng@gmail.com>
2019-12-17 01:09:51 +0300

    first commit

---
+++ instaclonefirabese/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  instaclonefirabese
+//
+//  Created by MacxbookPro on 27.11.2019.
+//  Copyright © 2019 MacxbookPro. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
